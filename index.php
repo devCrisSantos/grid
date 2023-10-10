e8066ffecb998c70d2387635eae5d588cd27c5d6
Cristiano <144123999+devCrisSantos@users.noreply.github.com>
2023-10-10 00:41:44 -0300

    Add
    Upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <title>Grid Sistemas</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">	
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>	
+	<div style="z-index: 2; position: absolute; top: 10px; left: 20px;" class="login-wrap p-3 p-md-4">
+	<center><img src="grid.png" width="170"></div><br><br>
+	<div class="container">			
+		<div class="login-wrap p-4 p-md-5">			
+			<table width=350 align=center><tr><td height=200 valign=center align=center>
+				<a href='../grid-psp'<br><img src="logoPsp.png" width="280"><br><font color=#006600 size=4>Programação para o Serviço de Pioneiro</font></a>
+			</td><tr><tr><td border height=200 width=350 align=center>
+				<a href='../grid-cdc'<br><img src="logoCdc.png" width="280"><br><font color=#993399 size=4>Controle de Designações Congregacionais</font></a>
+			</td></tr></table><br>
+		</div>
+		<center><b>&copy; GRID Sistemas - 
+		<? 
+			if(date('Y')==2023) echo date('Y');
+			else echo "2023 - ".date('Y');
+		?>
+		- Todos os direitos reservados</b>
+	</div>	    
+</body>
+</html>
