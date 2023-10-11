94108132a78b5f1e19b807acb24697d61133fa9a
Cristiano <144123999+devCrisSantos@users.noreply.github.com>
2023-10-11 13:41:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">	
-	<link rel="stylesheet" href="css/style.css">
-	<style>
+	<link rel="stylesheet" href="css/style.css"> 
+    <style>
         .borda-arredondada {
             width: 200px;
-            height: 200px;
-            background-color: #3498db; /* Cor de fundo da div */
-            border-radius: 20px; /* Define um raio para as bordas, criando um efeito arredondado */
+            height: 70px;
+            background-color: #ffffff; /* Cor de fundo da div */           
+            border-radius: 30px; /* Define um raio para as bordas, criando um efeito arredondado */
             text-align: center;
             color: #fff; /* Cor do texto dentro da div */
             line-height: 200px; /* Centraliza verticalmente o texto */
@@ -21,13 +21,13 @@
     </style>
 </head>
 <body>	
-	<div style="z-index: 2; position: absolute; top: 3px; left: 40px;" class="borda-arredondada"><center><img src="grid.png" width="180"></div>
-    	<div class="login-wrap p-2 p-md-1" style="position: absolute; top:13px; left:10px; width:98%">
-        <table width="98%" align=center><tr><td align=right>Olá usuário</td></tr></table>
-    </div><br><br><br><br>	
-		<div class="container">			
+	
+    <div style="z-index: 2; position: relative; top: 3px; left: 1%;" class="borda-arredondada"><img style="position: absolute; top: 10px; left: 10px;" src="grid.png" width="180"></div>
+    <table style="border-bottom: 1px solid gray; position: absolute; top:16px; right:30px; width:90%;"><tr><td align=right>Olá usuário</td></tr></table>
+    <br><br><br><br>
+	<div class="container">			
 		<div class="login-wrap p-4 p-md-5">			
-			<table width=350 align=center><tr><td height=200 valign=center align=center>
+			<table align=center><tr><td height=200 valign=center align=center>
 				<a href='../grid-psp'<br><img src="logoPsp.png" width="280"><br><font color=#006600 size=4>Programação para o Serviço de Pioneiro</font></a>
 			</td><tr><tr><td border height=200 width=350 align=center>
 				<a href='../grid-cdc'<br><img src="logoCdc.png" width="280"><br><font color=#993399 size=4>Controle de Designações Congregacionais</font></a>
